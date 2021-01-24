ca3c74f441353b1b477568330dd74544d502aa05
Luke James <ljgecko1230@gmail.com>
2021-01-23 19:03:57 -0800

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -90,7 +90,6 @@ async def handle_reminders():
                         now = datetime.datetime(now.year, now.month, now.day, now.hour - 8, now.minute)
                     date = parse_date(event["date"])
                     now_date_difference = date - now
-                    print(now_date_difference.seconds)
                     ref_event = Event(games[event["game"]], date, event["reminders"])
                     message = ""
                     hour_12 = date.hour
@@ -131,7 +130,7 @@ async def handle_reminders():
                         message += f"{' '.join([role.mention for role in ref_event.game.roles])}\n{ref_event.game.name} starts in {round(now_date_difference.seconds / 60)} minutes!"
                         await send(message, channel=current_server.announcement_channel)
                         updated_events.append(ref_event)
-                    elif now_date_difference.days == 0 and now.minute >= date.minute:
+                    elif now.year >= date.year and now.month >= date.month and now.day >= date.day and now.minute >= date.minute:
                         if not len(ref_event.game.roles):
                             message += "@everyone"
                         message += f"{' '.join([role.mention for role in ref_event.game.roles])}\n{ref_event.game.name} starts now!"
